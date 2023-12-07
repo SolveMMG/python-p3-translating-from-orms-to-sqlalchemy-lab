a3e2c1dff712e27b0afe6dfbf8e0de9b1ad27fce
Your Name <mikachupichu@gmail.com>
2023-12-07 11:55:51 +0300

    Completed assingment

--- lib/models.py
+++ lib/models.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import (Column, String, Integer)
+from sqlalchemy import (Column, String, Integer, create_engine)
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
+
 class Dog(Base):
+    engine = create_engine("sqlite:///:memory:")
+    Session =  sessionmaker(bind=engine)
+    session =  Session()
     __tablename__ = 'dogs'
 
     id = Column(Integer(), primary_key=True)
